bb588a5087f504b595d752e9003ea022b9a6c6da
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-01-30 00:00:44 -0800

    Removed logging configuration

--- Spirit/__init__.py
+++ Spirit/__init__.py
@@ -1,13 +1,2 @@
 import logging
-from logging.handlers import RotatingFileHandler
-
 logging.basicConfig(level=logging.DEBUG)
-
-logger = logging.getLogger("Spirit")
-
-universalHandler = RotatingFileHandler("spirit.log", maxBytes=2097152, backupCount=3, encoding="utf-8")
-logger.addHandler(universalHandler)
-
-errorHandler = logging.FileHandler("error.log")
-errorHandler.setLevel(logging.ERROR)
-logger.addHandler(errorHandler)
