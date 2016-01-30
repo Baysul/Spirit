ca1ddc72ef532bc99e252e9dabb84e48b9b50039
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-01-30 00:01:30 -0800

    Logger is configured based on values from the server's configuration

--- Spirit/Spirit.py
+++ Spirit/Spirit.py
@@ -1,5 +1,8 @@
 import logging
+from logging.handlers import RotatingFileHandler
+
 import json
+import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -23,6 +26,24 @@ class Spirit(Factory, object):
 		self.server = self.config["Servers"][serverName]
 
 		self.protocol = Penguin if self.server["World"] else Spheniscidae
+
+		# Set up logging
+		generalLogDirectory = os.path.dirname(self.server["Logging"]["General"])
+		errorsLogDirectory = os.path.dirname(self.server["Logging"]["Errors"])
+
+		if not os.path.exists(generalLogDirectory):
+			os.mkdir(generalLogDirectory)
+
+		if not os.path.exists(errorsLogDirectory):
+			os.mkdir(errorsLogDirectory)
+
+		universalHandler = RotatingFileHandler(self.server["Logging"]["General"],
+		                                       maxBytes=2097152, backupCount=3, encoding="utf-8")
+		self.logger.addHandler(universalHandler)
+
+		errorHandler = logging.FileHandler(self.server["Logging"]["Errors"])
+		errorHandler.setLevel(logging.ERROR)
+		self.logger.addHandler(errorHandler)
 
 		# TODO - make the engine string retrieve the values from the configuration attribute
 		self.databaseEngine = create_engine('mysql://root@localhost/spirit', pool_recycle=3600)
