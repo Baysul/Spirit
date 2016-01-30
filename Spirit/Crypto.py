7af0ac9c1cd140108b5d6fd80c96488518d4d024
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-01-29 21:18:38 -0800

    Fixed getLoginHash and added method for creating MD5 strings

--- Spirit/Crypto.py
+++ Spirit/Crypto.py
@@ -5,21 +5,30 @@ from string import ascii_letters, digits
 class Crypto:
 
 	@staticmethod
+	def hash(string):
+		if isinstance(string, (int, long)):
+			string = str(string)
+
+		return hashlib.md5(string.encode("utf-8")).hexdigest()
+
+	@staticmethod
 	def generateRandomKey():
 		characterSelection = ascii_letters + digits
 
-		return "".join(choice(characterSelection) for i in range(16))
+		return "".join(choice(characterSelection) for _ in range(16))
 
 	@staticmethod
-	def encryptPassword(password):
-		hash = hashlib.md5(password.encode('utf-8')).hexdigest()
-		swappedHash = hash[16:32] + hash[0:16]
+	def encryptPassword(password, hash=True):
+		if hash:
+			password = Crypto.hash(password)
+
+		swappedHash = password[16:32] + password[0:16]
 
 		return swappedHash
 
 	@staticmethod
 	def getLoginHash(password, rndK):
-		key = Crypto.encryptPassword(password).upper()
+		key = Crypto.encryptPassword(password, False)
 		key += rndK
 		key += "a1ebe00441f5aecb185d0ec178ca2305Y(02.>'H}t\":E1_root"
 
