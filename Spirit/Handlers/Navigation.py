6132f101af1ca72092bd4304f832a1d3ccf6923e
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-01-30 17:54:00 -0800

    A more complete revision of handleJoinWorld

--- Spirit/Handlers/Navigation.py
+++ Spirit/Handlers/Navigation.py
@@ -4,6 +4,8 @@ worldHandlers = {
 }
 
 def handleJoinWorld(self, data):
+	from time import time
+
 	self.logger.debug("Received joinWorld request")
 
 	playerId = data[4]
@@ -13,14 +15,21 @@ def handleJoinWorld(self, data):
 		self.logger.warn("User sent an invalid player id!")
 		self.transport.loseConnection()
 
-	if self.user.LoginKey == loginKey:
-		self.logger.debug("User's login key is valid!")
-
-	else:
+	if self.user.LoginKey != loginKey:
 		self.logger.warn("User sent invalid login key in joinWorld request!")
 
 	self.user.LoginKey = None
 	self.session.commit()
+
+	self.sendLine("%xt%js%-1%1%0%{0}%1%".format("1" if self.user.Moderator else "0"))
+
+	playerString = self.getPlayerString()
+	loginTime = time()
+
+	# TODO - update?
+	loadPlayer = "{0}|%{1}%0%1440%{2}%{3}%0%7521%%7%1%0%211843".format(playerString, self.user.Coins, str(loginTime),
+	                                                                   self.age)
+	self.sendLine("%xt%lp%-1%{0}%".format(loadPlayer))
 
 def handleJoinRoom(self, data):
 	self.logger.debug("Received joinRoom request")
