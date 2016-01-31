065fa06fb6f63aff1d952060f34933913046d608
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-01-31 13:48:18 -0800

    Implementations of handleBuyInventory and handleGetInventoryList

--- Spirit/Handlers/Item.py
+++ Spirit/Handlers/Item.py
@@ -1,8 +1,42 @@
 worldHandlers = {
-	"i#gi": "handleGetInventoryList"
+	"i#gi": "handleGetInventoryList",
+	"i#ai": "handleBuyInventory"
 }
 
-def handleGetInventoryList(self, data):
-	self.logger.debug("Handling inventory list request")
+def handleBuyInventory(self, data):
+	itemId = data[4]
 
-	self.sendLine("%xt%gi%-1%" + self.user.Inventory + "%")
+	self.logger.debug("Purchasing item {0}".format(itemId))
+
+	if not itemId.isdigit():
+		return self.sendError(402)
+
+	itemId = int(itemId)
+
+	if itemId not in self.spirit.items:
+		return self.sendError(402)
+
+	elif itemId in self.inventory:
+		return self.sendError(400)
+
+	itemCost = self.spirit.items[itemId]
+
+	if self.user.Coins < itemCost:
+		return self.sendError(401)
+
+	self.addItem(itemId)
+
+
+def handleGetInventoryList(self, data):
+	inventoryArray = self.user.Inventory.split("%")
+
+	try:
+		inventoryArray = map(int, inventoryArray)
+		self.inventory = inventoryArray
+
+	except ValueError:
+		self.logger.debug("Empty inventory string (?)")
+		self.inventory = []
+
+	finally:
+		self.sendLine("%xt%gi%-1%" + self.user.Inventory + "%")
