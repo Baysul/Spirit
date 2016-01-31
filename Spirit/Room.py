cad52b8a8562e0f5ff91afeefd2771e3d66f3d2e
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-01-30 18:20:33 -0800

    Initial implementation of the Room class

---
+++ Spirit/Room.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class Room(object):
+
+	def __init__(self, *room):
+		self.externalId, self.internalId = room
+
+		self.players = deque()
+
+	def send(self, data):
+		for player in self.players:
+			player.sendLine(data)
+
+	def generateRoomString(self):
+		roomString = "%".join([player.generatePlayerString() for player in self.players])
+
+		return roomString
+
+	def add(self, player):
+		self.players.append(player)
+
+		joinRoom = "%xt%jr%{0}%{1}%{2}%".format(self.internalId, self.externalId, self.generateRoomString())
+		addPlayer = "%xt%ap%{0}%{1}%".format(self.internalId, self.generateRoomString())
+
+		player.sendLine(joinRoom)
+		player.sendLine(addPlayer)
+
+		player.room = self
+
+	def remove(self, player):
+		self.players.remove(player)
+
+		self.send("%xt%rp%-1%{0}%".format(player.id))
