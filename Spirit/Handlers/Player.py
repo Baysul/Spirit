fcb3a42c15acfd227af282777cbcefc97c60026c
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-01-31 17:38:47 -0800

    Implementation of handleGetPlayerInfoById

--- Spirit/Handlers/Player.py
+++ Spirit/Handlers/Player.py
@@ -1,7 +1,34 @@
 worldHandlers = {
 	"u#sp": "handleSendPlayerMove",
-	"u#h": "handleSendHeartbeat"
+	"u#h": "handleSendHeartbeat",
+	"u#pbi": "handleGetPlayerInfoById"
 }
+
+def handleGetPlayerInfoById(self, data):
+	from sqlalchemy.orm import load_only
+	from ..Data.User import User
+
+	playerId = data[4]
+
+	if playerId.isdigit():
+		playerId = int(playerId)
+
+		if playerId == self.user.Id:
+			playerSwid = self.user.Swid
+			username = self.user.Username
+
+		else:
+			playerModel = self.session.query(User).options(load_only("Username", "Swid"))\
+				.filter_by(Id=playerId).first()
+
+			if playerModel is None:
+				return
+
+			username = playerModel.Username
+			playerSwid = playerModel.Swid
+
+		playerInfoById = "%xt%pbi%{0}%{1}%{2}%{3}%".format(self.room.internalId, playerSwid, playerId, username)
+		self.sendLine(playerInfoById)
 
 def handleSendHeartbeat(self, data):
 	self.sendLine("%xt%h%-1%")
