af948fed6398e84eb9175def9df699f4b1e0ae94
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2016-02-07 23:44:46 -0800

    Changed the name of the test plugin to Example

--- Spirit/Plugins/Example/__init__.py
+++ Spirit/Plugins/Example/__init__.py
@@ -5,12 +5,12 @@ import zope.interface
 from .. import Plugin
 from ...Events import Events
 
-class Test(object):
+class Example(object):
 	zope.interface.implements(Plugin)
 
 	author = "Spirit Team"
 	version = 1.0
-	description = "A plugin to verify plugin system functionality"
+	description = "A plugin to verify plugin system functionality and demonstrate implementation"
 
 	def __init__(self):
 		self.logger = logging.getLogger("Spirit")
@@ -20,8 +20,8 @@ class Test(object):
 		self.events.on("j#js", self.handleJoinWorld)
 
 	def handleJoinWorld(self, player, data):
-		self.logger.info("[Test] Received joinWorld data")
+		self.logger.info("[Example] Received joinWorld data")
 		print data
 
 	def ready(self):
-		self.logger.info("Test plugin is ready!")
+		self.logger.info("Example plugin is ready!")
